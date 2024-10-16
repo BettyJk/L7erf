c57c965303a89b822a5e880c59fbf64a474d8e14
BettyJK <jouakboutayna2003@gmail.com>
2024-10-16 22:51:40 +0100

    Railway

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-import logging
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG  
-)
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.info(f"Received /start from {update.effective_user.username}")
-    await update.message.reply_text("Salam, je suis l7erf bot, l'Ensamien qui va vous aider")
+    await update.message.reply_text("Salam, je suis l7erf bot ! Je vais vous aider.")
 
 def main():
-    try:
-        logging.info("Initializing bot...")
-        app = ApplicationBuilder().token("8088453308:AAGqgkEXVcRU0VnNIMyDY_mATnDnmuL7Zr8").build()
-        app.add_handler(CommandHandler("start", start))
-        logging.info("Bot is running... Waiting for commands.")
-        app.run_polling()
-    except Exception as e:
-        logging.error(f"Error occurred: {e}")
+    bot_token = os.getenv("BOT_TOKEN")  # Use environment variable
+    app = ApplicationBuilder().token(bot_token).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
